45d42ab6f59eb6b079a31981013d76e47095335d
Catacc0421 <catalina.clavijoc@uqvirtual.edu.co>
2024-08-29 06:52:03 -0500

    se colocaron los datos en el main de registrar cliente
    se crearon 3 clientes, y se eliminó uno y se volvió a mostrar la lista

--- gimnasio/src/main/java/gimnasio/app/GimnasioApp.java
+++ gimnasio/src/main/java/gimnasio/app/GimnasioApp.java
@@ -3,7 +3,18 @@ package gimnasio.app;
 import gimnasio.model.Gimnasio;
 
 public class GimnasioApp {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+
+        Gimnasio gimnasio = new Gimnasio();
+        gimnasio.registrarCliente("Mauricio","Barrio los álamos", "1238", "mauro@gmail.com", "1234567", "3218042309");
+        gimnasio.registrarCliente("Alisson","Barrio los quindos", "1509", "alisson@gmail.com", "0987654", "3238880349");
+        gimnasio.registrarCliente("Maria","Barrio bosques de pinares", "3108", "maria@gmail.com", "1234689", "3118347251");
+
+        gimnasio.imprimirClientes();
+
+        gimnasio.eliminarCliente("3108");
+
+        gimnasio.imprimirClientes();
 
     }
 }
--- gimnasio/src/main/java/gimnasio/model/Gimnasio.java
+++ gimnasio/src/main/java/gimnasio/model/Gimnasio.java
@@ -400,4 +400,5 @@ public class Gimnasio {
 
 
 
+
 }
