8564ff3d0875604d09df678c805a14776317d7d2
IvanDagc <ivand.guerreroc@uqvirtual.edu.co>
2024-08-29 20:44:25 -0500

    correccion palabra a los Ivan

--- gimnasio/src/main/java/gimnasio/app/GimnasioApp.java
+++ gimnasio/src/main/java/gimnasio/app/GimnasioApp.java
@@ -6,7 +6,7 @@ public class GimnasioApp {
     public static void main(String[] args) throws Exception {
 
         Gimnasio gimnasio = new Gimnasio("Gym 24/7", "Av 19N", "87278287");
-        gimnasio.registrarCliente("Mauricio","Barrio la álamos", "1238", "mauro@gmail.com", "1234567", "3218042309");
+        gimnasio.registrarCliente("Mauricio","Barrio los álamos", "1238", "mauro@gmail.com", "1234567", "3218042309");
         gimnasio.registrarCliente("Alisson","Barrio los quindos", "1509", "alisson@gmail.com", "0987654", "3238880349");
         gimnasio.registrarCliente("Maria","Barrio bosques de pinares", "3108", "maria@gmail.com", "1234689", "3118347251");
 
