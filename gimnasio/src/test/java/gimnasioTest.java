00caf560165b6501ecf24c1a3dae87a5633682f5
wilder-reyes10 <142546882+wilder-reyes10@users.noreply.github.com>
2024-08-30 22:45:54 -0500

    se agrega otra prueba unitaria

--- gimnasio/src/test/java/gimnasioTest.java
+++ gimnasio/src/test/java/gimnasioTest.java
@@ -1,5 +1,6 @@
 import gimnasio.model.Gimnasio;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -8,10 +9,8 @@ public class gimnasioTest {
     @Test
     public void registroClienteTest(){
 
-
         //Se crea una instancia de la clase Gimnasio
         Gimnasio gimnasio = new Gimnasio("Gym 24/7", "calle 19A", "3235659878");
-
 
         //Se verifica que no se lance ninguna excepción al registrar un cliente
         assertDoesNotThrow(() -> {
@@ -28,5 +27,20 @@ public class gimnasioTest {
 
 
     }
+
+    @Test
+    public void eliminarClienteTest() throws Exception {
+
+        Gimnasio gimnasio = new Gimnasio("Gym 24/7", "calle 19A", "3235659878");
+        // Se agrega un usuario al banco
+        gimnasio.registrarCliente("Juan Perez", "Calle 123", "654321", "juan@gmail.com", "123");
+
+        // Se elimina el usuario
+        gimnasio.eliminarCliente("654321");
+
+        // Se verifica que el usuario haya sido eliminado
+        Assertions.assertTrue(gimnasio.getListaClientes().isEmpty());
+    }
+
 }
 
